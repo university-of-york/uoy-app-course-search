cb16aa266af2ac8ee3dc985382fc8c1368d5bdc6
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-09-08 17:29:29 +0100

    CFIN-219: Add simple test for demonstration purposes
    In Next.js, test files can't live in the "pages" folder, so I have amended the folder structure slightly and moved "pages" and a new "tests" folder into "src"

---
+++ src/tests/index.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import App from "../pages/index";
+
+// basic test to demonstrate how to get started with app testing
+describe("App", () => {
+
+    it("renders without crashing", () => {
+        render(<App />);
+        expect(
+            screen.getByRole("heading", { name: "Hello World!" })
+        ).toBeInTheDocument();
+    });
+
+});
