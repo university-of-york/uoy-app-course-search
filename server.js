790105948577582c420be464b90bb2be76950d33
Nicola Normandale <nicola.normandale@york.ac.uk>
2020-09-11 15:44:19 +0100

    CFIN-206 Add resources to run application on AWS
    * This will deploy next.js app to a lambda with an express web server handling http requests

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const dev = process.env.NODE_ENV !== 'production'
+const next = require('next')
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const { parse } = require('url')
+const pathMatch = require('path-match')
+
+const server = express()
+const route = pathMatch()
+server.use('/_next', express.static(path.join(__dirname, '.next')))
+server.get('*', (req, res) => handle(req, res))
+
+module.exports = server
