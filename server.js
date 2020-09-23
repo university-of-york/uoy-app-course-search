4aa0c925e02c81578cd02afcdadb1685ef653ea2
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-09-23 12:40:37 +0100

    CFIN-290: Add comments to clarify server routing

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = next({});
 const handle = app.getRequestHandler();
 
 const server = express();
+// serve static assets directly from the build-time-generated ".next" folder
 server.use("/_next", express.static(path.join(__dirname, ".next")));
+// serve all other requests with Next
 server.get("*", (req, res) => handle(req, res));
 
 module.exports = server;
