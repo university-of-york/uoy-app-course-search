e43073d4880b18c5609a25e1adc330c2393ae569
Chris Evans <chris.evans@york.ac.uk>
2020-10-02 10:53:20 +0100

    CFIN-276 update comment again in next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // Retrieve assets from the correct API Gateway Stage in AWS.
+  // Retrieve assets from the correct URL that accounts for the Stage when running in AWS API Gateway.
   assetPrefix: isProd ? '/v1' : '',
 }
