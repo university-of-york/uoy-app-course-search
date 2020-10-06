ae8b8b0b62534a5e73c3a28da102194ec9624e5b
Chris Evans <chris.evans@york.ac.uk>
2020-10-06 10:45:00 +0100

    CFIN-303 add a basic search bar and button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
+    FormElement,
+    FormFieldset,
+    FormInputText,
+    Grid,
+    GridBoxFull,
+    GridRow,
+    SearchIcon,
     UniversityFooter,
     UniversityHeaderWithSearch,
     UniversityTitleBar,
@@ -17,6 +24,37 @@ const App = ({ isSuccessfulSearch, searchResults }) => {
             <UniversityTitleBar title="Courses" />
 
             <div className="o-wrapper o-wrapper--main o-grid js-wrapper--main">
+                <GridRow>
+                    <GridBoxFull>
+                        <form
+                            autoComplete="off"
+                            method="get"
+                            className="c-form c-form--joined"
+                            role="search"
+                            aria-label="Courses"
+                        >
+                            <FormFieldset>
+                                <Grid>
+                                    <GridRow>
+                                        <GridBoxFull>
+                                            <FormElement>
+                                                <FormInputText name="search" type="text" ariaLabel="Search" />
+                                                <button
+                                                    className="c-btn c-btn--medium"
+                                                    type="submit"
+                                                    aria-label="Search"
+                                                >
+                                                    <SearchIcon />
+                                                </button>
+                                            </FormElement>
+                                        </GridBoxFull>
+                                    </GridRow>
+                                </Grid>
+                            </FormFieldset>
+                        </form>
+                    </GridBoxFull>
+                </GridRow>
+
                 <div className="o-grid__row">
                     <div className="o-grid__box o-grid__box--full">
                         <CourseSearchResults isSuccessfulSearch={isSuccessfulSearch} searchResults={searchResults} />
--- src/tests/pages/index.test.js
+++ src/tests/pages/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import App, { getServerSideProps } from "../../pages";
 
 beforeEach(() => {
@@ -10,6 +10,19 @@ describe("App", () => {
         render(<App />);
 
         expect(screen.getByRole("heading", { name: "Courses" })).toBeInTheDocument();
+    });
+
+    it("displays the search element with a search input and button", () => {
+        render(<App />);
+
+        const courseSearchBox = screen.getByRole("search", { name: "Courses" });
+
+        expect(courseSearchBox).toBeVisible();
+
+        const { getByRole } = within(courseSearchBox);
+
+        expect(getByRole("textbox", { name: "Search" })).toBeInTheDocument();
+        expect(getByRole("button", { name: "Search" }));
     });
 
     it("displays the titles from course search results", () => {
