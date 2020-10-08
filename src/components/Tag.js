ff22d726ecc45e2f07a90a988b92e6c8b23d59c5
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-10-08 12:17:30 +0100

    CFIN-307: Icon markup tags are self-closing

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,7 +4,7 @@ import React from "react";
 const Tag = ({ icon, mainText }) => (
     <li className="c-tag-list__item">
         <span className="c-tag">
-            <i aria-hidden className={`c-tag__icon c-icon c-icon--${icon}`} data-testid="tag-icon"></i>
+            <i aria-hidden className={`c-tag__icon c-icon c-icon--${icon}`} data-testid="tag-icon" />
             {mainText}
         </span>{" "}
     </li>
