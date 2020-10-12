d53f245f593e6df7704f6ad9f66fd84f4a37d35a
Nicola Normandale <nicola.normandale@york.ac.uk>
2020-10-12 11:03:45 +0100

    CFIN-327 Display award as part of title

--- src/components/Course.js
+++ src/components/Course.js
@@ -6,7 +6,9 @@ const Course = ({ course }) => {
     return (
         <li className="c-listings-item ">
             <a className="c-listings-item__link" href={course.liveUrl}>
-                <h2 className="c-listings-item__title">{course.title}</h2>
+                <h2 className="c-listings-item__title">
+                    {course.title} {course.award}
+                </h2>
             </a>
             <CourseDetails course={course} />
         </li>
