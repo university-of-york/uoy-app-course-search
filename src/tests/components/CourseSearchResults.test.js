079901be6f8169921c128b344223370ae280daea
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2020-11-03 17:32:32 +0000

    CFIN-384: Display no search results when no search terms entered

--- src/tests/components/CourseSearchResults.test.js
+++ src/tests/components/CourseSearchResults.test.js
@@ -8,14 +8,14 @@ describe("CourseSearchResults", () => {
             { title: "Physics", liveUrl: "http://foo.baz" },
         ];
 
-        render(<CourseSearchResults isSuccessfulSearch searchResults={searchResults} />);
+        render(<CourseSearchResults isSuccessfulSearch searchResults={searchResults} searchTerm="foobar" />);
 
         expect(screen.getByRole("link", { name: "Maths" })).toBeInTheDocument();
         expect(screen.getByRole("link", { name: "Physics" })).toBeInTheDocument();
     });
 
     it("displays an appropriate message when the course search failed", () => {
-        render(<CourseSearchResults isSuccessfulSearch={false} />);
+        render(<CourseSearchResults isSuccessfulSearch={false} searchTerm="foobar" />);
 
         expect(screen.getByText(/Course search is currently unavailable. Please try again later/)).toBeInTheDocument();
         expect(screen.getByRole("link", { name: "contact IT Support" })).toBeInTheDocument();
@@ -26,8 +26,19 @@ describe("CourseSearchResults", () => {
     });
 
     it("displays an appropriate message when there are no search results returned", () => {
-        render(<CourseSearchResults isSuccessfulSearch searchResults={[]} />);
+        render(<CourseSearchResults isSuccessfulSearch searchResults={[]} searchTerm="foobar" />);
 
         expect(screen.getByText(/Sorry, your search did not return any results/)).toBeVisible();
+    });
+
+    it.each`
+        description    | searchTerm
+        ${"blank"}     | ${""}
+        ${"null"}      | ${null}
+        ${"undefined"} | ${undefined}
+    `("displays nothing when user has entered $description search terms", ({ searchTerm }) => {
+        render(<CourseSearchResults searchTerm={searchTerm} />);
+
+        expect(screen.queryByText(/./)).not.toBeInTheDocument();
     });
 });
