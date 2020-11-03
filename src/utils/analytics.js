c0c0a4c7c3ddfdfd4a2ee7d2823ba1f8117ef9de
Christian Beddows <christian.beddows@york.ac.uk>
2020-11-03 11:58:36 +0000

    CFIN-130 Fix lint errors

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactGA from "react-ga";
 
 const GA_TRACKING_ID = "UA-149035954-1";
@@ -16,7 +15,7 @@ const logPageView = () => {
 
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
-    console.debug("GA|Pageview Sent: ", window.location.pathname);
+    console.debug("GA|Pageview Sent:", window.location.pathname);
 };
 
 export { logPageView };
