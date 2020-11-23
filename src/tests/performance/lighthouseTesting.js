114db73247237c228757385a41d5fc95727a70ec
Christian Beddows <christian.beddows@york.ac.uk>
2020-11-23 15:48:08 +0000

    CFIN-391 Add lighthouse testing script & workflow

---
+++ src/tests/performance/lighthouseTesting.js
@@ -0,0 +1,43 @@
+const average = require("lodash.mean");
+const chromeLauncher = require("chrome-launcher");
+const lighthouse = require("lighthouse");
+
+const TEST_URL = "http://localhost:3000?search=maths";
+const ITERATIONS = 3;
+const MINIMUM_ACCEPTABLE_SCORE = 90;
+
+(async () => {
+    const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+
+    const options = {
+        logLevel: "info",
+        output: "html",
+        onlyCategories: ["performance", "accessibility"],
+        port: chrome.port,
+    };
+
+    let scores = [];
+
+    for (let i = 0; i < ITERATIONS; i++) {
+        const result = await lighthouse(TEST_URL, options);
+
+        scores.push(result.lhr.categories.performance.score * 100);
+    }
+
+    const averagePerformanceScore = average(scores);
+
+    await chrome.kill();
+
+    if (averagePerformanceScore >= MINIMUM_ACCEPTABLE_SCORE) {
+        console.log("Average performance score: " + averagePerformanceScore);
+        process.exit(0);
+    } else {
+        console.error(
+            "Average performance score of " +
+                averagePerformanceScore +
+                " is less than the minimum threshold of " +
+                MINIMUM_ACCEPTABLE_SCORE
+        );
+        process.exit(1);
+    }
+})();
