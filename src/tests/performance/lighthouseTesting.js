4bf350c227ec12776af1fefc1e6224cadcc0560e
Christian Beddows <christian.beddows@york.ac.uk>
2020-11-24 10:53:18 +0000

    CFIN-391 Update workflow to check both desktop and mobile

--- src/tests/performance/lighthouseTesting.js
+++ src/tests/performance/lighthouseTesting.js
@@ -2,38 +2,47 @@ const actions = require("@actions/core");
 const average = require("lodash.mean");
 const chromeLauncher = require("chrome-launcher");
 const lighthouse = require("lighthouse");
+const mobileConfig = require("lighthouse/lighthouse-core/config/lr-mobile-config");
+const desktopConfig = require("lighthouse/lighthouse-core/config/lr-desktop-config");
 
 const TEST_URL = "http://localhost:3000?search=maths";
-const ITERATIONS = 3;
-const MINIMUM_ACCEPTABLE_SCORE = 90;
+const ITERATIONS = 5;
+const MINIMUM_DESKTOP_SCORE = 90;
+const MINIMUM_MOBILE_SCORE = 80;
 
 (async () => {
+    const isMobileReport = process.argv.includes("--mobile");
     const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
 
     const options = {
         logLevel: "info",
         output: "html",
-        onlyCategories: ["performance", "accessibility"],
+        onlyCategories: ["performance"],
         port: chrome.port,
     };
+
+    const config = isMobileReport ? mobileConfig : desktopConfig;
+    const threshold = isMobileReport ? MINIMUM_MOBILE_SCORE : MINIMUM_DESKTOP_SCORE;
 
     let scores = [];
 
     for (let i = 0; i < ITERATIONS; i++) {
-        const result = await lighthouse(TEST_URL, options);
+        const result = await lighthouse(TEST_URL, options, config);
 
         scores.push(result.lhr.categories.performance.score * 100);
     }
 
     const averagePerformanceScore = average(scores);
 
+    console.log(scores);
+
     await chrome.kill();
 
-    if (averagePerformanceScore >= MINIMUM_ACCEPTABLE_SCORE) {
+    if (averagePerformanceScore >= threshold) {
         actions.info(`Average performance score: ${averagePerformanceScore}`);
     } else {
         actions.setFailed(
-            `Average performance score of ${averagePerformanceScore} is less than the minimum threshold of ${MINIMUM_ACCEPTABLE_SCORE}`
+            `Average performance score of ${averagePerformanceScore} is less than the minimum threshold of ${threshold}`
         );
     }
 })();
