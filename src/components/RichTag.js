6a856fed59b716e0efa2a83c8dd94ba08384c33e
Nicola Normandale <nicola.normandale@york.ac.uk>
2021-01-20 14:57:16 +0000

    CFIN-402 Remove ArrayTag definition and references as not currently being used * Stored in branch CFIN-371_with_ArrayTag_definition

--- src/components/RichTag.js
+++ src/components/RichTag.js
@@ -15,34 +15,4 @@ RichTag.propTypes = {
     subText: PropTypes.string,
 };
 
-const ArrayTag = ({ title, content }) => {
-    const contents = content.map((item) => (
-        <ArrayTagContent key={item.text} icon={item.icon} label={item.text} fullWidth={title === null} />
-    ));
-
-    return (
-        <li className="rich-tag">
-            {title && <p className="rich-tag__title">{title}</p>}
-            <ul className="c-icon--ul">{contents}</ul>
-        </li>
-    );
-};
-
-ArrayTag.propTypes = {
-    title: PropTypes.string,
-    content: PropTypes.array,
-};
-
-const ArrayTagContent = ({ icon, label }) => (
-    <li className="rich-tag__text">
-        {icon && <i aria-hidden className={`c-icon c-icon--${icon} c-icon--1g c-icon--li`} data-testid={icon} />}
-        {label}
-    </li>
-);
-
-ArrayTagContent.propTypes = {
-    icon: PropTypes.string,
-    label: PropTypes.string,
-};
-
-export { RichTag, ArrayTag };
+export { RichTag };
--- src/tests/components/RichTagList.test.js
+++ src/tests/components/RichTagList.test.js
@@ -1,6 +1,6 @@
 import { render, screen, within } from "@testing-library/react";
 import { RichTagList } from "../../components/RichTagList";
-import { ArrayTag, RichTag } from "../../components/RichTag";
+import { RichTag } from "../../components/RichTag";
 import React from "react";
 
 describe("TagList", () => {
@@ -24,48 +24,5 @@ describe("TagList", () => {
 
         expect(tags[2]).toHaveTextContent("hot pudding");
         expect(within(tags[2]).getByTestId("tag-icon")).toHaveClass("c-icon--plane");
-    });
-
-    it("displays extra tags as per inputs", () => {
-        render(
-            <RichTagList>
-                <ArrayTag
-                    title="Extras"
-                    content={[
-                        { icon: "suitcase", text: "year abroad" },
-                        { icon: "plane", text: "year in industry" },
-                        { icon: "mortar-board", text: "Distance Learning" },
-                    ]}
-                />
-                <ArrayTag
-                    title=""
-                    content={[
-                        { icon: "suitcase", text: "year abroad" },
-                        { icon: "plane", text: "year in industry" },
-                        { icon: "mortar-board", text: "Distance Learning" },
-                        { icon: "mortar-board", text: "Integrated Masters" },
-                    ]}
-                />
-            </RichTagList>
-        );
-
-        const tags = screen.getAllByRole("listitem");
-
-        expect(tags[0]).toHaveTextContent("Extras");
-        expect(tags[0]).toHaveTextContent("year abroad");
-        expect(tags[0]).toHaveTextContent("year in industry");
-        expect(within(tags[0]).getByTestId("suitcase")).toHaveClass("c-icon--suitcase");
-        expect(within(tags[0]).getByTestId("plane")).toHaveClass("c-icon--plane");
-        expect(within(tags[0]).getByTestId("mortar-board")).toHaveClass("c-icon--mortar-board");
-
-        expect(tags[4]).not.toHaveTextContent("Extras");
-        expect(tags[4]).toHaveTextContent("year abroad");
-        expect(tags[4]).toHaveTextContent("year in industry");
-        expect(tags[4]).toHaveTextContent("Distance Learning");
-        expect(tags[4]).toHaveTextContent("Integrated Masters");
-        expect(within(tags[4]).getByTestId("suitcase")).toHaveClass("c-icon--suitcase");
-        expect(within(tags[4]).getByTestId("plane")).toHaveClass("c-icon--plane");
-        const mortarBoards = within(tags[4]).getAllByTestId("mortar-board");
-        expect((mortarBoards.length = 2));
     });
 });
