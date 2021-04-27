ae49cbf17b9ba40be1ea4748757ba414c001f1e9
Richard Allum <richard.allum@york.ac.uk>
2021-04-27 09:08:03 +0100

    CFIN-284 Add GlobalNotice for feedback

---
+++ src/tests/components/GlobalNotice.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { GlobalNotice } from "../../components/GlobalNotice";
+
+describe("GloablNotice", () => {
+    it("renders child content as expected", () => {
+        const children = <h2>This is a notice</h2>;
+
+        const { asFragment } = render(<GlobalNotice>{children}</GlobalNotice>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("does not render any content if no children are present", () => {
+        render(<GlobalNotice />);
+
+        expect(screen.queryByText(/./)).not.toBeInTheDocument();
+    });
+});
