3ad31daf8c2483eac81833328947b01bba188db8
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2021-06-03 09:46:12 +0100

    CFIN-522: Install Google Tag Manager
    Following instructions from
    https://www.learnbestcoding.com/post/9/easiest-way-to-integrate-google-analytics-with-react-js-and-next-js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import "../../public/stylesheets/styles.css";
 import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 // Required for Next to use a global stylesheet - see README
 const CourseSearchApp = ({ Component, pageProps }) => {
+    useEffect(() => {
+        TagManager.initialize({ gtmId: "GTM-WXLX54" });
+    }, []);
     return <Component {...pageProps} />;
 };
 
