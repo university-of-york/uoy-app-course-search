feff92e7c6415a285cc163722d59544098c2ce41
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2021-06-10 14:44:24 +0100

    CFIN-524: Cache pages and static assets for 1 day
    These cache settings will be honoured by CloudFront once Course Search is hosted from www.york.ac.uk. It will mean that most users will be served a cached version of search results and static assets, which should lead to improved performance and better user experience.

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const url = require("url");
 const { applicationBasePath } = require("./src/constants/basePath");
 
 const server = express();
-// serve static assets directly from the build-time-generated ".next" folder
-server.use(`${applicationBasePath}/_next`, express.static(path.join(__dirname, ".next")));
+// serve static assets directly from the build-time-generated ".next" folder and specify how long to cache
+server.use(
+    `${applicationBasePath}/_next`,
+    express.static(path.join(__dirname, ".next"), { maxAge: "1d", immutable: true })
+);
 // serve all other application requests with Next
-server.get(applicationBasePath, (req, res) => handle(req, res));
+server.get(applicationBasePath, (req, res) => {
+    res.setHeader("Cache-Control", "public, max-age=86400");
+    handle(req, res);
+});
 // redirect users from "/" to the application
 server.get("/", (req, res) =>
     res.redirect(
