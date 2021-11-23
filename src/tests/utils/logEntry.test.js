e6d6576d0674a6477e04c2158cea8f2685fa2fd0
rmil <rhys.milling@york.ac.uk>
2021-11-23 11:31:41 +0000

    CFIN-582: cover new cases in logEntry testing

--- src/tests/utils/logEntry.test.js
+++ src/tests/utils/logEntry.test.js
@@ -2,57 +2,59 @@ import MockDate from "mockdate";
 import { logEntry } from "../../utils/logEntry";
 import { StatusCodes } from "http-status-codes";
 
+const event = {
+    method: "GET",
+    headers: {
+        Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "CloudFront-Forwarded-Proto": "https",
+        "CloudFront-Is-Desktop-Viewer": "true",
+        "CloudFront-Is-Mobile-Viewer": "false",
+        "CloudFront-Is-SmartTV-Viewer": "false",
+        "CloudFront-Is-Tablet-Viewer": "false",
+        "CloudFront-Viewer-Country": "GB",
+        Host: "k0fqbcuftg.execute-api.eu-west-1.amazonaws.com",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
+        Via: "2.0 bf4a364e1dd23fe6634f1bf013457c5c.cloudfront.net (CloudFront)",
+        "X-Amz-Cf-Id": "7Px4kURZfIuh4B09Wv6vM5cSpbVXaOv1pW6h0eBMnHPpsJVQ19QtuA==",
+        "X-Amzn-Trace-Id": "Root=1-60a6734a-06f7e1dd276e8dc64be2d129",
+        "X-Forwarded-For": "144.32.100.16, 130.176.97.157",
+        "X-Forwarded-Port": "443",
+        "X-Forwarded-Proto": "https",
+    },
+    statusCode: 200,
+    requestContext: {
+        identity: {
+            sourceIp: "144.32.100.16",
+        },
+        apiId: "theApiId",
+    },
+};
+
+const parameters = {
+    search: "biology",
+};
+
+const details = {
+    results: [],
+};
+
+const error = new Error("test error");
+
 describe("Request Logging", () => {
-    it("Logs all relevant data when all is provided", () => {
+    it("logs all relevant data when all is provided", () => {
         MockDate.set(new Date());
-        const event = {
-            method: "GET",
-            headers: {
-                Accept:
-                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-                "CloudFront-Forwarded-Proto": "https",
-                "CloudFront-Is-Desktop-Viewer": "true",
-                "CloudFront-Is-Mobile-Viewer": "false",
-                "CloudFront-Is-SmartTV-Viewer": "false",
-                "CloudFront-Is-Tablet-Viewer": "false",
-                "CloudFront-Viewer-Country": "GB",
-                Host: "k0fqbcuftg.execute-api.eu-west-1.amazonaws.com",
-                "sec-ch-ua-mobile": "?0",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "none",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-                Via: "2.0 bf4a364e1dd23fe6634f1bf013457c5c.cloudfront.net (CloudFront)",
-                "X-Amz-Cf-Id": "7Px4kURZfIuh4B09Wv6vM5cSpbVXaOv1pW6h0eBMnHPpsJVQ19QtuA==",
-                "X-Amzn-Trace-Id": "Root=1-60a6734a-06f7e1dd276e8dc64be2d129",
-                "X-Forwarded-For": "144.32.90.155, 130.176.97.157",
-                "X-Forwarded-Port": "443",
-                "X-Forwarded-Proto": "https",
-            },
-            statusCode: 200,
-            requestContext: {
-                identity: {
-                    sourceIp: "144.32.90.155",
-                },
-                apiId: "theApiId",
-            },
-        };
 
-        const queryStringParameters = {
-            search: "biology",
-        };
-
-        const additionalDetails = {
-            results: [],
-        };
-
-        expect(logEntry(event, queryStringParameters, additionalDetails)).toEqual({
-            clientIp: "144.32.90.155",
+        expect(logEntry(event, parameters, details)).toEqual({
+            clientIp: "144.32.100.16",
             parameters: {
                 search: "biology",
             },
@@ -60,15 +62,41 @@ describe("Request Logging", () => {
         });
     });
 
-    it("Puts nonexistent fields as null instead of skipping them", () => {
-        const event = {
-            queryStringParameters: {
-                search: "biology",
-            },
-        };
-
-        const result = logEntry(event, StatusCodes.OK);
+    it("puts nonexistent fields as null instead of skipping them", () => {
+        const result = logEntry(null, parameters, null);
 
         expect(result.clientIp).toBeNull();
+    });
+
+    it("can log an error", () => {
+        const result = logEntry(null, null, null, error);
+
+        expect(result.error).toEqual(error);
+    });
+
+    it("can parse the http event to find an IP address", () => {
+        const result = logEntry(event, null, null);
+
+        expect(result.clientIp).toEqual("144.32.100.16");
+    });
+
+    it("can handle with no http request", () => {
+        const result = logEntry(null, parameters, details);
+
+        expect(result.clientIp).toBeNull();
+    });
+
+    it("can handle with no http request and an error", () => {
+        const result = logEntry(null, parameters, details, error);
+
+        expect(result.clientIp).toBeNull();
+        expect(result.error).toEqual(error);
+    });
+
+    it("can handle with a http request and an error", () => {
+        const result = logEntry(event, parameters, details, error);
+
+        expect(result.clientIp).toEqual("144.32.100.16");
+        expect(result.error).toEqual(error);
     });
 });
