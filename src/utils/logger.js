c1ec48527b5e076486441d57feda827aeaa122b0
rmil <rhys.milling@york.ac.uk>
2021-11-25 12:12:17 +0000

    CFIN-582: remove `application` field

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -16,15 +16,6 @@ const logger = pino({
     serializers: {
         error: pino.stdSerializers.err,
     },
-    // Base controls what fields are included in the JSON
-    // object, by default Pino includes fields like pid which
-    // are not very relevant in a serverless context
-    //
-    // We haven't included all fields here as `logEntry` will
-    // add the rest otherwise the keys will duplicate
-    base: {
-        application: "uoy-app-course-search",
-    },
     // Whilst we could use `pino.stdTimeFunctions.isoTime`, it doesn't
     // have a key of `timestamp` which our logging format requires
     timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
