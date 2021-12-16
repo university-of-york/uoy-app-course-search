70fdf48a2d80aa1b5e97e84b252e5e6859df915e
rmil <rhys.milling@york.ac.uk>
2021-12-16 17:34:07 +0000

    CFIN-593: update course type

--- src/constants/CourseModel.js
+++ src/constants/CourseModel.js
@@ -1,11 +1,16 @@
 import PropTypes from "prop-types";
 
 const COURSE_MODEL = PropTypes.shape({
-    length: PropTypes.string,
-    title: PropTypes.string,
-    liveUrl: PropTypes.string,
-    award: PropTypes.string,
-    yearOfEntry: PropTypes.string,
+    ucasCode: PropTypes.string.isRequired,
+    length: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    liveUrl: PropTypes.string.isRequired,
+    award: PropTypes.string.isRequired,
+    yearOfEntry: PropTypes.string.isRequired,
+    inClearingOnlyHome: PropTypes.bool,
+    inClearingOnlyInternational: PropTypes.bool,
+    inAdjustmentOnlyHome: PropTypes.bool,
+    inAdjustmentOnlyInternational: PropTypes.bool,
 });
 
 export { COURSE_MODEL };
